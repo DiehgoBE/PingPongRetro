39c42ab2dd41defd8ec1cb82af03fd84d0bc93c7
Diego Bravo <diegobraes21@gmail.com>
2020-11-22 18:21:59 -0500

    Velocidad y colisiones

--- Ping Pong Retro/Assets/Scripts/BallBehaviour.cs
+++ Ping Pong Retro/Assets/Scripts/BallBehaviour.cs
@@ -6,7 +6,8 @@ public class BallBehaviour : MonoBehaviour
 {
 
     public Transform paddle;
-    bool gameStarted = false;   
+    bool gameStarted = false;
+    public Rigidbody2D rbBall;
     
     // Start is called before the first frame update
     void Start()
@@ -21,9 +22,10 @@ public class BallBehaviour : MonoBehaviour
         {
             float posDif = paddle.position.x - transform.position.x;
             transform.position = new Vector3(paddle.position.x - posDif,paddle.position.y,paddle.position.z);
-
+           
             if (Input.GetMouseButtonDown(0))
             {
+                rbBall.velocity = new Vector2(8, 8);
                 gameStarted = true;
             }
 
