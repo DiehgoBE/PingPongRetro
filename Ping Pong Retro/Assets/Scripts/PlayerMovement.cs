63b6d06e8edd7c5c833b6940beef62898748ab7f
Diego Bravo <diegobraes21@gmail.com>
2020-11-22 12:01:04 -0500

    Script Movimiento del Paddle

---
+++ Ping Pong Retro/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z);
+
+
+    }
+}
