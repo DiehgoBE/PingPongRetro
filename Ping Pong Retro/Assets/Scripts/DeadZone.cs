6e3e317596415a7d0a56bbac057ea4df7f47a65e
Diego Bravo <diegobraes21@gmail.com>
2020-12-30 09:22:22 -0500

    Despliegue Pong Retro

--- Ping Pong Retro/Assets/Scripts/DeadZone.cs
+++ Ping Pong Retro/Assets/Scripts/DeadZone.cs
@@ -1,11 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class DeadZone : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    //Variables
+    public Text scorePlayerText;
+    public Text scoreEnemyText;
+
+    int scorePlayerQuantity;
+    int scoreEnemyQuantity;
+
+    public SceneChanger sceneChanger;
+
+    private void OnTriggerEnter2D(Collider2D ball)
     {
-        Debug.Log("Trigger");
+        if (gameObject.tag == "Left")
+        {
+            scoreEnemyQuantity++;
+            UpdateScoreLabel(scoreEnemyText, scoreEnemyQuantity);
+        }else if (gameObject.CompareTag("Right"))
+        {
+            scorePlayerQuantity++;
+            UpdateScoreLabel(scorePlayerText, scorePlayerQuantity);
+        }
+
+        ball.GetComponent<BallBehaviour>().gameStarted = false;
+
+        CheckScore();
+
+        GetComponent<AudioSource>().Play();
+    }
+
+    void CheckScore()
+    {
+        if (scorePlayerQuantity >= 3)
+        {
+            sceneChanger.ChangeSceneTo("WinScene");
+        }else if (scoreEnemyQuantity >= 3)
+        {
+            sceneChanger.ChangeSceneTo("LoseScene");
+        }
+    }
+
+    void UpdateScoreLabel(Text label, int score)
+    {
+        label.text = score.ToString();
     }
 }
---
+++ Ping Pong Retro/Assets/Scripts/FollowBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBall : MonoBehaviour
+{
+    public Transform Ball;
+    public float speed;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Ball.GetComponent<BallBehaviour>().gameStarted)
+        {
+            if (transform.position.y < Ball.position.y)
+            {
+                transform.position = new Vector3(transform.position.x,transform.position.y + speed, transform.position.z);
+            }else if (transform.position.y > Ball.position.y)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+            }
+
+        }
+    }
+}
